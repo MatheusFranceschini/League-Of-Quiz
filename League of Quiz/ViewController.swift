1aaa487c72bcd681e9d4ae7d8044181cedb7be8e
MatheusFranceschini <masuzena@gmail.com>
2025-03-05 14:32:11 -0300

    Initial Commit

---
+++ League of Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  League of Quiz
+//
+//  Created by Matheus Franceschini on 05/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
